a4eb2eea927601147f2ad65978043af7cbfecd02
VivienIngrams <vivingrams@gmail.com>
2023-07-12 11:39:27 +0100

    Fix padding on Footer

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -3,12 +3,12 @@ import {FC, memo} from 'react';
 import Socials from '../Socials';
 
 const Footer: FC = memo(() => (
-  <div className="fixed bottom-0 w-full bg-neutral-900/50 -my-5 px-2 sm:px-8 sm:pt-5 sm:pb-8">
+  <div className="fixed bottom-0 w-full bg-neutral-900/50 px-4 py-3 sm:px-8 sm:pt-5 sm:pb-5">
     <div className="flex flex-col items-center gap-y-2">
       <div className="flex gap-x-2 text-neutral-300">
         <Socials />
       </div>
-      <span className="text-sm text-neutral-100">sakiko_oishi@yahoo.com</span>
+      <span className="text-sm text-neutral-100 ">sakiko_oishi@yahoo.com</span>
     </div>
   </div>
 ));
--- src/pages/CV/index.tsx
+++ src/pages/CV/index.tsx
@@ -11,37 +11,37 @@ const {actions} = heroData;
 const CV: FC = memo(() => {
   return (
     <>
-    <Header />
-    <Section className="bg-neutral-100" sectionId={SectionId.CV}>
-    <div className="flex w-full justify-center gap-x-4">
-                {actions.map(({href, text, primary, Icon}) => (
-                  <a
-                    className={classNames(
-                      'flex gap-x-2 rounded-full border-2 bg-none py-2 px-4 text-sm font-medium text-red-900 ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
-                      primary ? 'border-gray-900 ring-gray-900' : 'border-red-900 ring-red-900',
-                    )}
-                    href={href}
-                    key={text}>
-                    {text}
-                    {Icon && <Icon className="h-5 w-5 text-red-900 sm:h-6 sm:w-6" />}
-                  </a>
-                ))}
-              </div>{' '}
-      <div className="flex flex-col divide-y-2 divide-neutral-300">
-        <ResumeSection title="Work Experience">
-          {experience.map((item, index) => (
-            <TimelineItem item={item} key={`${item.title}-${index}`} />
+      <Header />
+      <Section className="bg-neutral-100" sectionId={SectionId.CV}>
+        <div className="flex w-full justify-center gap-x-4 py-20 ">
+          {actions.map(({href, text, primary, Icon}) => (
+            <a
+              className={classNames(
+                'border-2bg-none bg-neutral-700/50 flex gap-x-2 rounded-full py-2 px-4 text-sm font-medium text-red-900 ring-offset-gray-700/80 hover:bg-neutral-400/50 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
+                primary ? 'border-gray-900 ring-gray-900' : 'border-red-900 ring-red-900',
+              )}
+              href={href}
+              key={text}>
+              {text}
+              {Icon && <Icon className="h-5 w-5 text-red-900 sm:h-6 sm:w-6" />}
+            </a>
           ))}
-        </ResumeSection>
-        <ResumeSection title="Training">
-          {education.map((item, index) => (
-            <TimelineItem item={item} key={`${item.title}-${index}`} />
-          ))}
-        </ResumeSection>
-      
-      </div>
-    </Section>
-    <Footer /></>
+        </div>{' '}
+        <div className="flex flex-col divide-y-2 divide-neutral-300">
+          <ResumeSection title="Work Experience">
+            {experience.map((item, index) => (
+              <TimelineItem item={item} key={`${item.title}-${index}`} />
+            ))}
+          </ResumeSection>
+          <ResumeSection title="Training">
+            {education.map((item, index) => (
+              <TimelineItem item={item} key={`${item.title}-${index}`} />
+            ))}
+          </ResumeSection>
+        </div>
+      </Section>
+      <Footer />
+    </>
   );
 });
 
@@ -52,7 +52,7 @@ const ResumeSection: FC<PropsWithChildren<{title: string}>> = memo(({title, chil
   return (
     <div className="grid grid-cols-1 gap-y-4 py-8 first:pt-0 last:pb-0  md:grid-cols-4">
       <div className="col-span-1 flex justify-center md:justify-start">
-        <div className="relative h-max mb-5">
+        <div className="relative mb-5 h-max">
           <h2 className="text-xl font-bold uppercase text-orange-800 ">{title}</h2>
           <span className="absolute inset-x-0 -bottom-1 border-b-2 border-orange-800" />
         </div>
@@ -63,7 +63,6 @@ const ResumeSection: FC<PropsWithChildren<{title: string}>> = memo(({title, chil
 });
 
 ResumeSection.displayName = 'ResumeSection';
-
 
 // eslint-disable-next-line import/first
 import {TimelineItem} from '../../data/dataDef';
