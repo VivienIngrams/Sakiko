4bbeef57ba97acd9e342f2af157c24e012564d9f
VivienIngrams <vivingrams@gmail.com>
2023-07-12 11:04:48 +0100

    Fixing app

--- src/pages/CV/index.tsx
+++ src/pages/CV/index.tsx
@@ -1,12 +1,10 @@
 import classNames from 'classnames';
-import {FC, memo} from 'react';
+import {FC, memo, PropsWithChildren} from 'react';
 
 import Section from '../../components/Layout/Section';
 import Footer from '../../components/Sections/Footer';
 import Header from '../../components/Sections/Header';
 import {education, experience, heroData, SectionId} from '../../data/data';
-import ResumeSection from './ResumeSection';
-import TimelineItem from './TimelineItem';
 
 const {actions} = heroData;
 
@@ -49,3 +47,42 @@ const CV: FC = memo(() => {
 
 CV.displayName = 'Resume';
 export default CV;
+
+const ResumeSection: FC<PropsWithChildren<{title: string}>> = memo(({title, children}) => {
+  return (
+    <div className="grid grid-cols-1 gap-y-4 py-8 first:pt-0 last:pb-0  md:grid-cols-4">
+      <div className="col-span-1 flex justify-center md:justify-start">
+        <div className="relative h-max mb-5">
+          <h2 className="text-xl font-bold uppercase text-orange-800 ">{title}</h2>
+          <span className="absolute inset-x-0 -bottom-1 border-b-2 border-orange-800" />
+        </div>
+      </div>
+      <div className="col-span-1 flex flex-col md:col-span-3">{children}</div>
+    </div>
+  );
+});
+
+ResumeSection.displayName = 'ResumeSection';
+
+
+// eslint-disable-next-line import/first
+import {TimelineItem} from '../../data/dataDef';
+
+const TimelineItem: FC<{item: TimelineItem}> = memo(({item}) => {
+  const {title, date, location, content} = item;
+  return (
+    <div className="flex flex-col pb-8 text-center last:pb-0 md:text-left">
+      <div className="flex flex-col pb-4">
+        <h2 className="text-xl font-bold">{title}</h2>
+        <div className="flex items-center justify-center gap-x-2 md:justify-start">
+          <span className="flex-1 text-sm font-medium italic sm:flex-none">{location}</span>
+          <span>•</span>
+          <span className="flex-1 text-sm sm:flex-none">{date}</span>
+        </div>
+      </div>
+      {content}
+    </div>
+  );
+});
+
+TimelineItem.displayName = 'TimelineItem';
