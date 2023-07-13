25bcd80292b8c8916064a264014ac3b9a260420b
VivienIngrams <vivingrams@gmail.com>
2023-07-13 15:59:38 +0100

    Created dynamic routing for gallery pages

--- src/pages/gallery/[photos].tsx
+++ src/pages/gallery/[photos].tsx
@@ -1,21 +1,42 @@
 import classNames from 'classnames';
 import Image from 'next/image';
+import {useRouter} from 'next/router';
 import {FC, memo} from 'react';
 
 import Section from '../../components/Layout/Section';
 import Footer from '../../components/Sections/Footer';
 import Header from '../../components/Sections/Header';
-import {galleryItemsModel,SectionId} from '../../data/data';
+import {galleryItemsDance, galleryItemsHands, galleryItemsModel, galleryItemsStage, SectionId} from '../../data/data';
+import {GalleryItem} from '../../data/dataDef';
 
 const Model: FC = memo(() => {
+  const router = useRouter();
+
+  let section: GalleryItem[] = [];
+
+  switch (router.query.photos) {
+    case 'dance':
+      section = galleryItemsDance;
+      break;
+    case 'model':
+      section = galleryItemsDance;
+      break;
+    case 'stage':
+      section = galleryItemsStage;
+      break;
+    case 'hands':
+      section = galleryItemsHands;
+      break;
+  }
+
   return (
     <>
       <Header />
       <Section className="bg-black" sectionId={SectionId.Gallery}>
         <div className="mb-20 flex flex-col gap-y-8">
-          <h2 className="self-center text-xl font-thin uppercase  text-white">Model</h2>
+          <h2 className="self-center text-xl font-thin uppercase  text-white">{section}</h2>
           <div className=" w-full columns-2 md:columns-3 lg:columns-4">
-            {galleryItemsModel.map((item, index) => {
+            {section.map((item, index) => {
               const {image} = item;
               return (
                 <div className="pb-6" key={`${index}`}>
@@ -36,7 +57,6 @@ const Model: FC = memo(() => {
     </>
   );
 });
-
 
 Model.displayName = 'Model';
 export default Model;
