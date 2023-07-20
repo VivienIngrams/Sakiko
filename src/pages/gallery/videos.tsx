dd878d81a7c2612842e599da616d2460af97c5ed
VivienIngrams <vivingrams@gmail.com>
2023-07-20 14:18:48 +0100

    Using video data from mongodb

--- src/pages/gallery/videos.tsx
+++ src/pages/gallery/videos.tsx
@@ -1,12 +1,24 @@
 import classNames from 'classnames';
+import {MongoClient} from 'mongodb';
 import {FC, memo} from 'react';
 
 import Section from '../../components/Layout/Section';
 import Footer from '../../components/Sections/Footer';
 import Header from '../../components/Sections/Header';
-import {galleryItemsVideos, SectionId} from '../../data/data';
+import {SectionId} from '../../data/data';
 
-const Videos: FC = memo(() => {
+const password: string = process.env.MONGODB_PASSWORD || '';
+const encodedPassword: string = encodeURIComponent(password);
+
+interface videoProps {
+  videoData: {
+    src: string;
+    title: string;
+    id: string;
+  }[];
+}
+
+const Videos: FC<videoProps> = memo((props) => {
   return (
     <>
       <Header />
@@ -14,7 +26,7 @@ const Videos: FC = memo(() => {
         <div className="mb-20 flex flex-col items-center gap-y-8">
           <h2 className="self-center text-xl font-thin uppercase text-white">Videos</h2>
           <div className="grid max-w-full grid-cols-1 gap-y-8 ">
-            {galleryItemsVideos.map((item, index) => {
+            {props.videoData.map((item, index) => {
               const {src} = item;
               return (
                 <div className="w-full" key={`${index}`}>
@@ -41,3 +53,37 @@ const Videos: FC = memo(() => {
 
 Videos.displayName = 'Videos';
 export default Videos;
+
+
+export async function getStaticProps() {
+  try {
+    const client = await MongoClient.connect(
+      `mongodb+srv://vivien:${encodedPassword}@cluster0.9j3scal.mongodb.net/video?retryWrites=true&w=majority`,
+    );
+    const db = client.db();
+    const videoCollection = db.collection('video');
+
+    const videoData = await videoCollection.find().toArray();
+
+    client.close();
+
+    return {
+      props: {
+        videoData: videoData.map(videoEntry => ({
+          src: videoEntry.data.src,
+          title: videoEntry.data.title,
+          id: videoEntry._id.toString(),
+        })),
+      },
+      revalidate: 10000,
+    };
+  } catch (error) {
+    console.error('Error fetching data from MongoDB:', error);
+
+    return {
+      props: {
+        videoData: [],
+      },
+    };
+  }
+}
